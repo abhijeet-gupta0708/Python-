823af88304394d6c780ee512b706a9eb4021b9eb
Abhijeet Gupta <abhijeet.sci02@gmail.com>
2025-10-01 00:35:10 +0530

    Strings in Python completed (RADHE RADHE)

---
+++ string.py
@@ -0,0 +1,51 @@
+# problem 1 
+# print name followed by good morning
+
+
+
+# name=input("Enter name ")
+# print("Good morning dear",name)
+
+
+
+
+# problem 2-- print table
+
+
+
+# name=input("Enter your name")
+# date=input("Enter date in DDMMYYYY format")
+# print("Letter")
+# letter= ''' Dear <|Name|> ,
+#             You are selected
+#             <|date|>'''
+# print(letter)
+# final=letter.replace("<|Name|>",name)
+# finalletter=final.replace("<|date|>",date)
+# print(finalletter)
+
+
+
+
+# problem 3 find double spacing in string
+
+# star=input("Enter a string")
+# if "  "in star:
+#     print("There are double spacing at",star.find("  "))
+# else:
+#     print("There are no double spacing")
+
+
+
+
+# problem 4 - Replace the double spacing from problem 3 with single space
+
+
+# star=input("Enter a string")
+# if "  "in star:
+#     print("There are double spacing at",star.find("  "))
+# else:
+#     print("There are no double spacing")
+
+# print(star.replace("  "," "))
+
