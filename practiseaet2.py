f7785d42d6bcfb6dd2a65957ec128a472144da94
Abhijeet Gupta <abhijeet.sci02@gmail.com>
2025-09-30 23:05:58 +0530

    Initial commit

---
+++ practiseaet2.py
@@ -0,0 +1,26 @@
+# a=int(input("Enter 1st number"))
+# b=int(input("Enter 2nd number"))
+# c=a+b
+# print(c)
+
+# problem 2
+
+# a=int(input("Enter the Number you want to divide  "))
+# b=int(input("ENter the dividing number  "))
+# c=a/b
+# print(c)
+
+# problem 3
+# a=input("Enter something")
+# print(type(a))
+
+# //problem4
+
+# a=int(input("Enter a number"))
+# b=int(input("Enter a number"))
+# if(a>b):
+#     print(a)
+# else :
+#     print(b)
+
+# problem 5
