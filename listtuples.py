ab6b6ef59ad4f827bf3e9d1adee3dc251ed54848
Abhijeet Gupta <abhijeet.sci02@gmail.com>
2025-10-04 00:19:30 +0530

    List and Tuples for Python

---
+++ listtuples.py
@@ -0,0 +1,84 @@
+#Problem 1::Print 7 Marks names Entered by a user in a list
+
+
+# Marks=[]
+# f1=int(input("Enter the marks
+# )")
+# Marks.append(f1)
+# f2=int(input("Enter the marks
+# )")
+# Marks.append(f2)
+# f3=int(input("Enter the marks
+# )")
+# Marks.append(f3)
+# f4=int(input("Enter the marks
+# )")
+# Marks.append(f4)
+# f5=int(input("Enter the marks
+# )")
+# Marks.append(f5)
+# f6=int(input("Enter the marks
+# )")
+# Marks.append(f6)
+# f7=int(input("Enter the marks
+# )")
+# Marks.append(f7)
+# print(Marks)
+
+
+
+#problem 2::Accept the marks and store in sorted formate
+
+
+
+# Marks=[]
+# f1=int(input("Enter the marks "))
+# Marks.append(f1)
+# f2=int(input("Enter the marks "))
+# Marks.append(f2)
+# f3=int(input("Enter the marks "))
+# Marks.append(f3)
+# f4=int(input("Enter the marks "))
+# Marks.append(f4)
+# f5=int(input("Enter the marks "))
+# Marks.append(f5)
+# f6=int(input("Enter the marks "))
+# Marks.append(f6)
+# f7=int(input("Enter the marks "))
+# Marks.append(f7)
+# print(Marks)
+# print("marks in sorted order ",sorted(Marks))
+# Marks.sort()
+# print("marks in sorted order ",Marks)
+
+
+
+#problem 3::--Print the sum of a list
+
+
+# Marks=[]
+# f1=int(input("Enter the marks "))
+# Marks.append(f1)
+# f2=int(input("Enter the marks "))
+# Marks.append(f2)
+# f3=int(input("Enter the marks "))
+# Marks.append(f3)
+# f4=int(input("Enter the marks "))
+# Marks.append(f4)
+# f5=int(input("Enter the marks "))
+# Marks.append(f5)
+# f6=int(input("Enter the marks "))
+# Marks.append(f6)
+# f7=int(input("Enter the marks "))
+# Marks.append(f7)
+# print(Marks)
+# print("Sum of markd ",sum(Marks))
+
+
+
+
+#problem 4::Wrrite a program to cout zeros in following tuple(7,0,8,0,0,9)
+
+
+# tuple=(7,0,8,0,0,9)
+# print("no of zeros in given tuple ",tuple.count(0))
