1a2e85acfa9a7b1e979068c4da5df91d2fe4d74e
Abhijeet Gupta <abhijeet.sci02@gmail.com>
2025-10-05 01:20:02 +0530

    Dictionary and sets

---
+++ dictionary&set.py
@@ -0,0 +1,53 @@
+#problem ::1  Write a program to create a dictionary of Hindi words with values as their English translation  
+#                   Provide user with an option to look it up!
+
+
+
+
+# Hindi_words ={
+#     "suprabhat" :" Good morning",
+#     "Namaskarm" :"Hello",
+#     "Prem":"Love"
+# }
+# a=int(input("Press 1 to look dicitonary \n \t \t or \n press any other key to exit the program"))
+# if a==1 :
+#       print(Hindi_words)
+
+
+# else :
+#  print("Program exited")
+
+
+
+#Problem 2::Write a program to input eight numbers from the user and display all the unique  numbers (once).
+
+
+
+# s=set()
+# num1=int(input("Enter a number"))
+# s.add(num1)
+# num2=int(input("Enter a number"))
+# s.add(num2)
+# num3=int(input("Enter a number"))
+# s.add(num3)
+# num4=int(input("Enter a number"))
+# s.add(num4)
+# num5=int(input("Enter a number"))
+# s.add(num5)
+# num6=int(input("Enter a number"))
+# s.add(num6)
+# num7=int(input("Enter a number"))
+# s.add(num7)
+# num8=int(input("Enter a number"))
+# s.add(num8)
+# print(s)
+# print("Sorted unique value are",sorted(s))
+
+
+
+
+
+
+
+
+
