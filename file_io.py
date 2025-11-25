331a9f76d1064165a9595888eb6854dd79909b72
Abhijeet Gupta <abhijeet.sci02@gmail.com>
2025-11-25 23:18:19 +0530

    FILE INPUT - OUTPUT

---
+++ file_io.py
@@ -0,0 +1,32 @@
+# f=open("test.txt","r")
+# tesx=f.read()
+# print(tesx)
+
+
+
+'''Write a program to read the text from a given file ‘poems.txt’ and find out
+whether it contains the word ‘twinkle’.'''
+
+# f=open("poems.txt","r")
+# readtext=f.read()
+# print(readtext)
+# f.seek(0)
+# new=f.readline()
+# print(new)
+# f.seek(0)
+# print(f.readlines())
+# f.seek(0)
+# readtext.lower()
+# if "twinkle" in readtext :
+#     print("Yes")
+# count=readtext.count("twinkle")
+# print(count)
+
+
+
+
+'''The game() function in a program lets a user play a game and returns the score
+as an integer. You need to read a file ‘Hi-score.txt’ which is either blank or
+contains the previous Hi-score. You need to write a program to update the Hiscore whenever the game() function breaks the Hi-score'''
+
+
