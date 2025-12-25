93e9f1c9757b55c5bfb6f5c7b6d803123d513120
Abhijeet Gupta <abhijeet.sci02@gmail.com>
2025-12-25 16:38:01 +0530

    Classes and Objects in  Python

---
+++ class_objects.py
@@ -0,0 +1,120 @@
+'''1. Create a class “Programmer” for storing information of few programmers
+working at Microsoft.'''
+
+
+# name=input("Enter The name of the programmer to get his information :\n ")
+# class programmer:
+#     naam=name
+#     language ="Python"
+#     salary=160000
+#     Experience="0-2 years"
+#     company="Microsoft"
+
+
+# name=programmer()
+# name.names=name
+# print(f"The Programmer '{name.naam} 'has his Experties in '{name.language} 'having salary of '{name.salary}' per months or per week , and he has just the expeirence of '{name.Experience} 'and he just got his job as fresher from college in 3rd year of his B.Tech in the company '{name.company}'")
+
+
+
+
+'''2. Write a class “Calculator” capable of finding square, cube and square root of a
+self '''
+
+# number=int(input("Enter a Number"))
+# class calculator :
+#     square=number*number
+#     cube=number**3
+#     squareroot=number**(1/2)
+
+#     def calcu(self):
+#        print(self.square,self.cube,self.squareroot)
+
+# number=calculator()
+
+# number.calcu()
+
+
+
+
+
+
+'''3. Create a class with a class attribute a; create an object from it and set ‘a’
+directly using ‘object.a = 0’. Does this change the class attribute?'''
+
+
+
+# class atribute :
+#     a="object"
+
+# change=atribute ()
+# print(change.a)
+# change.a="changed"
+
+# print(change.a)
+
+
+
+
+
+
+'''Add a static method in problem 2, to greet the user with hello'''
+
+
+# number=int(input("Enter a Number"))
+# class calculator :
+#     square=number*number
+#     cube=number**3
+#     squareroot=number**(1/2)
+
+#     def calcu(self):
+#        print(self.square,self.cube,self.squareroot)
+
+#     @staticmethod
+#     def greet():
+#         print("Static method implied Hello")
+
+# number=calculator()
+
+# number.calcu()
+# number.greet()
+
+
+
+
+
+
+'''Write a Class ‘Train’ which has methods to book a ticket, get status (no of seats)
+and get fare information of train running under Indian Railways.
+'''
+
+# class Train :
+#     seats_number=100
+#     fare="₹100"
+#     info="Satabdi Express"
+#     train_no=12421
+
+
+    
+#     def Ticket_book(self,name):
+#         self.name=name
+#         self.train_no=int(input("Enter the Train no :   "))
+#         self.Booking_station=input("Enter Your Bokking station :  ")
+#         self.destination=input("Enter Your Destination :  ")
+        
+
+#     def printing(self,name):
+#         self.Ticket_book(name);
+        
+#         print(f"{self.name} is traveling from {self.Booking_station} to Their Destination {self.destination} in tarin no {self.train_no}  train name {self.info}having fare {self.fare} having total seats {self.seats}")
+
+
+
+
+# name=input("Enter Your name :  ")
+# T=Train()
+# T.printing(name)      
+
+
+
+
